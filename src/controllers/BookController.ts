ed24e25f14a1ca94c3d8673ffe80e870e94ec253
santiago.develop1 <santinhoandrade1@gmail.com>
2025-06-21 12:19:50 -0300

    Add addBook method to BookController for creating new books

--- src/controllers/BookController.ts
+++ src/controllers/BookController.ts
@@ -8,6 +8,12 @@ class BookController {
     const books = await this.bookRepository.findAll();
     res.json(books);
   }
+
+  addBook = async (req: Request, res: Response) => {
+    const book = await this.bookRepository.create(req.body);
+    res.json(book);
+  }
+
 }
 
 export default new BookController();
